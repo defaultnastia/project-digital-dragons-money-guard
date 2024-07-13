ab91585dab391aaa7ca171d6d0ba21397cb5b706
LieraBerh <lierrastern@gmail.com>
2024-07-13 14:52:53 +0200

    currency table mock

--- src/components/CurrencyTab/CurrencyTab.tsx
+++ src/components/CurrencyTab/CurrencyTab.tsx
@@ -1,5 +1,29 @@
 const CurrencyTab = () => {
-  return <div>CurrencyTab</div>;
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Currency</th>
+            <th>Purchase</th>
+            <th>Sale</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>USD</td>
+            <td>27.55</td>
+            <td>27.65</td>
+          </tr>
+          <tr>
+            <td>EUR</td>
+            <td>30.00</td>
+            <td>30.10</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default CurrencyTab;
