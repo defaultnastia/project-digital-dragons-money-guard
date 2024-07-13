71a74ddd587a93043237904578889175f6cd3443
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-13 12:09:58 +0300

    feat(app): Add structure for StatisticsTab

--- src/components/StatisticsTab/StatisticsTab.tsx
+++ src/components/StatisticsTab/StatisticsTab.tsx
@@ -1,5 +1,16 @@
+import Chart from "../Chart/Chart";
+import StatisticsDashboard from "../StatisticsDashboard/StatisticsDashboard";
+import StatisticsTable from "../StatisticsTable/StatisticsTable";
+
 const StatisticsTab = () => {
-  return <div>StatisticsTab</div>;
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <Chart />
+      <StatisticsDashboard />
+      <StatisticsTable />
+    </div>
+  );
 };
 
 export default StatisticsTab;
