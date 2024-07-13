ce57a7f5b06370e026e8806217295f8079d8717d
LieraBerh <lierrastern@gmail.com>
2024-07-13 14:20:31 +0200

    Create monobankApi.ts

---
+++ src/services/monobankApi.ts
@@ -0,0 +1,50 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+
+const monobankApi: AxiosInstance = axios.create({
+  baseURL: "https://api.monobank.ua/bank/currency",
+});
+
+interface CurrencyRate {
+  currencyCodeA?: number;
+  currencyCodeB?: number;
+  rateBy: number;
+  rateSell: number;
+  date?: number;
+}
+
+const STORAGE_KEY = "currencyRates";
+
+const getCurrentTime = (): number => new Date().getTime();
+
+const saveToLocalStorage = (data: CurrencyRate[]): void => {
+  const currentTime = getCurrentTime();
+  const dataToStore = { rates: data, timestamp: currentTime };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataToStore));
+};
+
+const getFromLocalStorage = (): CurrencyRate[] | null => {
+  const storedData = localStorage.getItem(STORAGE_KEY);
+  if (storedData) {
+    const { rates, timestamp } = JSON.parse(storedData);
+    const currentTime = getCurrentTime();
+    if (currentTime - timestamp < 3600000) {
+      return rates;
+    }
+  }
+  return null;
+};
+
+export const getCurrencyRates = async (): Promise<CurrencyRate[] | null> => {
+  const cachedRates = getFromLocalStorage();
+  if (cachedRates) {
+    return cachedRates;
+  }
+  try {
+    const response: AxiosResponse<CurrencyRate[]> = await monobankApi.get("/");
+    const currencyRates = response.data;
+    saveToLocalStorage(currencyRates);
+    return currencyRates;
+  } catch (err) {
+    return null;
+  }
+};
