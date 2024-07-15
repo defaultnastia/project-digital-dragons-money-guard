eb864b45fdfe6992d049bf78f7ff671c51074447
defaultnastia <anastayaaa@gmail.com>
2024-07-15 17:55:12 +0200

    fixed redux
    - fixed redux simultaneous loading issue
    - fixed error codes displaying
    - added 00 to the balance
    - added refresh action to the APP

--- src/redux/user/selectors.ts
+++ src/redux/user/selectors.ts
@@ -3,3 +3,4 @@ import { RootState } from "../store";
 export const selectUserData = (state: RootState) => state.user.user;
 export const selectLoadingState = (state: RootState) => state.user.loading;
 export const selectErrorCode = (state: RootState) => state.user.errorCode;
+export const selectIsLoggedIn = (state: RootState) => state.user.isLoggedIn;
