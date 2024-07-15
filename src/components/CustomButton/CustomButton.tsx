1f74e26a7aa3eb4bd29cfc1c1094205ccff44cc1
lukinivan <Lukinivan09@gmail.com>
2024-07-15 18:37:11 +0100

    Update CustomButton.tsx

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -10,7 +10,7 @@ type CustomButtonPropsType = {
 
 const style = {
   colorful:
-    "block mx-auto w-[100%] uppercase text-[18px] tracking-[0.1em] rounded-[20px] py-[13px] px-[20px] mb-[20px] md:w-[300px] bg-gradient-to-r from-[#FFC727] from-0% via-[#9E40BA] via-[61%] to-[#7000FF] to-[91%] drop-shadow-xl",
+    "block mx-auto w-[100%] uppercase text-center text-[18px] tracking-[0.1em] rounded-[20px] py-[13px] px-[20px] mb-[20px] md:w-[300px] bg-gradient-to-r from-[#ffc727] from-0% via-[#9e40ba] via-61% to-[#7000ff] to-90% drop-shadow-xl",
   mono: "block mx-auto w-[100%] uppercase text-center text-[18px] tracking-[0.1em] rounded-[20px] py-[13px] px-[20px] mb-[20px] bg-[var(--white-color)] text-[var(--text-button-color)] md:w-[300px]",
 };
 
