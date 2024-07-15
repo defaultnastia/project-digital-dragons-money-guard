14aadda2030e665d412ddec9e1063b46bad44be2
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-15 21:50:08 +0300

    feat(app): get date transaction

--- src/components/Chart/Chart.type.ts
+++ src/components/Chart/Chart.type.ts
@@ -3,9 +3,27 @@ type Data = {
   name?: string;
   type?: string;
   total?: number;
+  id?: string;
+  categoryId?: string;
+  date?: string;
 };
 
 type ColorSchema = {
   color: string;
   name?: string;
+  id?: string;
+  categoryId?: string;
+  date?: string;
+};
+
+type DateTransaction = {
+  [key: string]: any;
+};
+
+type Transaction = {
+  id?: string;
+  type?: string;
+  transactionDate?: string;
+  categoryId?: string;
+  date?: string;
 };
--- src/helpers/statistics/dataProcessing.tsx
+++ src/helpers/statistics/dataProcessing.tsx
@@ -1,22 +1,38 @@
 import {useEffect, useState} from "react";
-import {transactionType, transactionValue} from "../../components/StatisticsTab/temp/http";
+import {
+  transactionAll,
+  transactionType,
+  transactionValue,
+} from "../../components/StatisticsTab/temp/http";
 import {colorsStatistics} from "./colors";
+import {
+  getTransactionsCategories,
+  getTransactionsSummary,
+} from "../../redux/transactions/operations";
+import {useAppDispatch} from "../../redux/hooks";
 
 export const dataProcessing = () => {
-  const [nameTransactions, setNameTransactions] = useState<string[]>([]);
+  const [nameTransactions, setNameTransactions] = useState<Data[]>([]);
   const [value, setValue] = useState<Data[]>([]);
+  const [date, setDate] = useState<Transaction[]>([]);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    const http = async () => {
+    const fetchTransactionsCategories = async () => {
+      // const {payload: data} = await dispatch(getTransactionsCategories());
       const data = await transactionType();
       const itemName = [...data]
         .filter((item) => item.type !== "INCOME")
-        .map((property) => property.name);
+        .map((property) => {
+          return {name: property.name, id: property.id};
+        });
       setNameTransactions(itemName);
     };
 
-    const httpValue = async () => {
+    const fetchTransactionsSummary = async () => {
+      // const {payload: data} = await dispatch(getTransactionsSummary());
       const {categoriesSummary} = await transactionValue();
+      // const {categoriesSummary} = data;
       const transactions: Data[] = [...categoriesSummary]
         .filter((item) => item.type !== "INCOME")
         .map((item) => {
@@ -25,17 +41,42 @@ export const dataProcessing = () => {
       setValue(transactions);
     };
 
-    http();
-    httpValue();
-  }, []);
+    const fetchTransactions = async () => {
+      // const {payload: data} = await dispatch(getTransactionsSummary());
+      const data: Transaction[] = await transactionAll();
+      // const {categoriesSummary} = data;
+      const dates = data
+        .filter((item) => item.type !== "INCOME")
+        .map((item) => {
+          return {id: item.id, date: item.transactionDate, categoryId: item.categoryId};
+        });
 
-  const generalColorSchema: ColorSchema[] = colorsStatistics.map((name, index) => {
-    return {color: name, name: nameTransactions[index]};
+      setDate(dates);
+    };
+
+    fetchTransactionsCategories();
+    fetchTransactionsSummary();
+    fetchTransactions();
+  }, [dispatch]);
+
+  const generalColorSchema: ColorSchema[] = nameTransactions.map((transaction, index) => {
+    return {
+      color: colorsStatistics[index],
+      name: transaction.name,
+      id: transaction.id,
+    };
   });
 
   const dataTransaction: Data[] = value.map((transaction) => {
     const colorInfo = generalColorSchema.find((color) => color.name === transaction.name);
-    return {...transaction, color: colorInfo?.color || ""};
+    const dateInfo = date.find((date) => date.categoryId === colorInfo?.id);
+
+    return {
+      ...transaction,
+      color: colorInfo?.color || "",
+      id: colorInfo?.id || "",
+      date: dateInfo?.date || "",
+    };
   });
 
   return {dataTransaction};
