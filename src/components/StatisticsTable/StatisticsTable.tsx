72541fd13cc3ffb1ece8e3af239b1868b8d43718
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-15 21:50:44 +0300

    feat(app): base table transactions

--- src/components/StatisticsTable/StatisticsTable.tsx
+++ src/components/StatisticsTable/StatisticsTable.tsx
@@ -1,5 +1,33 @@
+import {dataProcessing} from "../../helpers/statistics/dataProcessing";
+
 const StatisticsTable = () => {
-  return <div>StatisticsTable</div>;
+  const {dataTransaction} = dataProcessing();
+  console.log(dataTransaction);
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Category</th>
+            <th>Sum</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataTransaction.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>
+                <span
+                  className={`bg-[${transaction.color}] w-[24px] h-[24px] block rounded-[2px]`}
+                ></span>
+                <p>{transaction.name}</p>
+              </td>
+              <td>{transaction.total}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default StatisticsTable;
