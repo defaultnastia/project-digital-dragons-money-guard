a61744677a02f1d71385ae3d21ae62b4b814a7c8
lukinivan <Lukinivan09@gmail.com>
2024-07-15 21:11:01 +0100

    Strict types added. hover, focus styles

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,37 +1,51 @@
+/* How To Use
+
+Button type
+<CustomButton elementLike={ {btnType: "submit" or "button", onClick={AnyHandleClickFunction} *not require type} *Object } btnStyle="colorful" or "mono"> Any Text *(require type) </CustomButton>
+
+Link type
+<CustomButton elementLike={ {linkTo: "your link"} *Object } btnStyle="colorful" or "mono"> Any Text *(require type) </CustomButton>
+
+*/
 import { Link } from "react-router-dom";
 
-type CustomButtonPropsType = {
-  btnStyle: "colorful" | "mono";
-  type: "submit" | "button";
+type ButtonLike = {
+  btnType: "submit" | "button";
   onClick?: () => void;
+};
+
+type LinkLike = {
+  linkTo: string;
+};
+
+type CustomButtonPropsType = {
+  elementLike: ButtonLike | LinkLike;
+  btnStyle: "colorful" | "mono";
   children: string;
-  linkTo?: string;
 };
 
 const style = {
   colorful:
-    "block mx-auto w-[100%] uppercase text-center text-[18px] tracking-[0.1em] rounded-[20px] py-[13px] px-[20px] mb-[20px] md:w-[300px] bg-gradient-to-r from-[#ffc727] from-0% via-[#9e40ba] via-61% to-[#7000ff] to-90% drop-shadow-xl",
-  mono: "block mx-auto w-[100%] uppercase text-center text-[18px] tracking-[0.1em] rounded-[20px] py-[13px] px-[20px] mb-[20px] bg-[var(--white-color)] text-[var(--text-button-color)] md:w-[300px]",
+    "transition-all duration-300 ease-in-out block mx-auto w-[100%] uppercase text-center text-[18px] tracking-[0.1em] rounded-[20px] py-[13px] px-[20px] mb-[20px] md:w-[300px] bg-gradient-to-r from-[#ffc727] from-0% via-[#9e40ba] via-61% to-[#7000ff] to-90% hover:bg-gradient-to-r hover:from-[#ffc527de] hover:via-[#9d40bada] hover:to-[#6f00ffd0] focus:bg-gradient-to-r focus:from-[#ffc527de] focus:via-[#9d40bada] focus:to-[#6f00ffd0] drop-shadow-xl",
+  mono: "block mx-auto w-[100%] uppercase text-center text-[18px] tracking-[0.1em] rounded-[20px] py-[13px] px-[20px] mb-[20px] bg-[var(--white-color)] hover:bg-gray-300 focus:bg-gray-300 transition duration-300 ease-in-out text-[var(--text-button-color)] md:w-[300px]",
 };
 
 export const CustomButton = ({
-  type,
-  linkTo,
-  onClick,
+  elementLike,
   btnStyle,
   children,
 }: CustomButtonPropsType) => {
-  return linkTo !== undefined ? (
+  return "linkTo" in elementLike ? (
     <Link
-      to={linkTo ? linkTo : "/"}
+      to={(elementLike as LinkLike).linkTo}
       className={btnStyle === "colorful" ? style.colorful : style.mono}
     >
       {children}
     </Link>
   ) : (
     <button
-      type={type}
-      onClick={onClick}
+      type={(elementLike as ButtonLike).btnType}
+      onClick={(elementLike as ButtonLike).onClick}
       className={btnStyle === "colorful" ? style.colorful : style.mono}
     >
       {children}
