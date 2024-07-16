fff9f6d3d92ef9ea4cb1adcdc27d1e88d506ae7f
LieraBerh <lierrastern@gmail.com>
2024-07-16 18:20:09 +0200

    add some styles

--- src/components/CurrencyTab/CurrencyTab.tsx
+++ src/components/CurrencyTab/CurrencyTab.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { CurrencyRate, getCurrencyRates } from "../../services/monobankApi";
 import s from "./CurrencyTab.module.css";
+import clsx from "clsx";
 
 const CurrencyTab: React.FC = () => {
   const [currencyRates, setCurrencyRates] = useState<CurrencyRate[] | null>(
@@ -40,22 +41,30 @@ const CurrencyTab: React.FC = () => {
   };
 
   return (
-    <div className="flex-col w-[336px] xl:w-[480px] bg-[rgba(74,86,226,0.10)]">
+    <div className="flex-col w-[336px] xl:w-[480px] bg-[rgba(74,86,226,0.1)] rounded-br-[8px] rounded-bl-[8px] xl:rounded-none overflow-hidden">
       {loading ? (
-        <p>Loading...</p>
+        <p className="flex items-center justify-center h-[109px]">Loading...</p>
       ) : error ? (
-        <p>Error fetching currency rates</p>
+        <p className="flex items-center justify-center h-[109px]">
+          Error fetching currency rates
+        </p>
       ) : (
-        <table className="text-[16px] w-full table-fixed xl:mb-[15px]">
+        <table className="text-[16px] w-full table-fixed xl:mb-[36px]">
           <thead className="text-[16px] bg-[rgba(255,255,255,0.20)] w-full">
             <tr>
-              <th className="pt-[13px] pb-[13px] pl-[20px]" align="left">
+              <th
+                className="pt-[13px] pb-[13px] pl-[20px] xl:pl-[96px]"
+                align="left"
+              >
                 Currency
               </th>
               <th className="pt-[13px] pb-[13px]" align="center">
                 Purchase
               </th>
-              <th className="pt-[13px] pb-[13px] pr-[20px]" align="right">
+              <th
+                className="pt-[13px] pb-[13px] pr-[20px] xl:pr-[96px]"
+                align="right"
+              >
                 Sale
               </th>
             </tr>
@@ -63,14 +72,29 @@ const CurrencyTab: React.FC = () => {
           <tbody>
             <tr className="h-[8px] bg-transparent"></tr>
             {currencyRates?.map((rate, index) => (
-              <tr key={index}>
-                <td className="pb-[12px] pl-[20px]" align="left">
+              <tr key={index} className={index === 1 ? s.noPaddingBottom : ""}>
+                <td
+                  className={clsx("pb-[12px] pl-[20px] xl:pl-[96px]", {
+                    [s.noBottomPadding]: index === 1,
+                  })}
+                  align="left"
+                >
                   {getCurrencyName(rate.currencyCodeA)}
                 </td>
-                <td className="pb-[12px]" align="center">
+                <td
+                  className={clsx("pb-[12px]", {
+                    [s.noBottomPadding]: index === 1,
+                  })}
+                  align="center"
+                >
                   {rate.rateBuy.toFixed(2)}
                 </td>
-                <td className="pb-[12px] pr-[20px]" align="right">
+                <td
+                  className={clsx("pb-[12px] pr-[20px] xl:pr-[96px]", {
+                    [s.noBottomPadding]: index === 1,
+                  })}
+                  align="right"
+                >
                   {rate.rateSell.toFixed(2)}
                 </td>
               </tr>
