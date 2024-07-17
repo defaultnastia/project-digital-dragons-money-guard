c223ab733c3cd0faea0750c149ebb6dfbd7ca51d
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-17 10:26:39 +0300

    fix(app): fix statistic

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,18 +1,24 @@
 import {Chart as ChartJS, ArcElement, Tooltip} from "chart.js";
 import {Doughnut} from "react-chartjs-2";
-import {dataProcessing} from "../../helpers/statistics/dataProcessing";
+
+import {colorsStatistics} from "../../helpers/statistics/colors";
+import NotificationStatistic from "../NotificationStatistic/NotificationStatistic";
 
 ChartJS.register(ArcElement, Tooltip);
 
-const Chart = () => {
-  const {dataTransaction} = dataProcessing();
+const Chart = ({dataTransaction, balance}: ChartProps) => {
+  if (!dataTransaction || !dataTransaction.categoriesSummary) {
+    return null;
+  }
+
+  const categoriesSummary = dataTransaction.categoriesSummary;
   const data = {
-    labels: dataTransaction.map((data) => data.name),
+    labels: categoriesSummary.map((data) => data.name),
     datasets: [
       {
-        data: dataTransaction.map((data) => data.total),
-        backgroundColor: dataTransaction.map((data) => data.color),
-        borderColor: dataTransaction.map((data) => data.color),
+        data: categoriesSummary.map((data) => -data.total),
+        backgroundColor: colorsStatistics,
+        borderColor: colorsStatistics,
         borderWidth: 1,
         cutout: "70%",
       },
@@ -33,8 +39,17 @@ const Chart = () => {
   };
 
   return (
-    <div>
-      <Doughnut data={data} plugins={[shadowPlugin]} />
+    <div className="mb-8 min-[768px]:w-[336px] min-[1280px]:w-[288px] grid place-items-center relative">
+      {categoriesSummary.length !== 0 ? (
+        <>
+          <Doughnut data={data} plugins={[shadowPlugin]} />
+          <p className="text-[1.125rem] font-semibold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            &#8372; {balance}
+          </p>
+        </>
+      ) : (
+        <NotificationStatistic text="No transactions for the selected date" />
+      )}
     </div>
   );
 };
--- src/components/StatisticsTable/StatisticsTable.tsx
+++ src/components/StatisticsTable/StatisticsTable.tsx
@@ -1,45 +1,58 @@
-import {dataProcessing} from "../../helpers/statistics/dataProcessing";
+import {nanoid} from "@reduxjs/toolkit";
+import {colorsStatistics} from "../../helpers/statistics/colors";
 
-const StatisticsTable = () => {
-  const {dataTransaction, totalExpense, totalIncome} = dataProcessing();
+const StatisticsTable = ({dataTransaction}: ChartProps) => {
+  if (!dataTransaction || !dataTransaction.categoriesSummary) {
+    return null;
+  }
+
+  const categoriesSummary = dataTransaction.categoriesSummary.map((category, index) => ({
+    ...category,
+    id: nanoid(),
+    color: colorsStatistics[index % colorsStatistics.length],
+  }));
+
+  const expenseSum =
+    dataTransaction.expenseSummary !== undefined ? -dataTransaction.expenseSummary : 0;
+  const incomeSum = dataTransaction.incomeSummary;
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Category</th>
-            <th>Sum</th>
-          </tr>
-        </thead>
-        <tbody>
-          {dataTransaction.map((transaction) => (
-            <tr key={transaction.id}>
-              <td>
-                <span
-                  className={`bg-[${transaction.color}] w-[24px] h-[24px] block rounded-[2px] opacity-[1]`}
-                >
-                  {transaction.color}
+    <div className="">
+      {categoriesSummary.length !== 0 && (
+        <div>
+          <div className="flex justify-between p-4 bg-[rgba(82,59,126,0.60)] rounded-lg mb-[14px]">
+            <p>Category</p>
+            <p>Sum</p>
+          </div>
+
+          <ul className="mb-4 text-[0.875rem]">
+            {categoriesSummary.map((item) => (
+              <li
+                key={item.id}
+                className="flex justify-between py-[12.4px] px-4  border-b border-[rgba(255,255,255,0.41)]"
+              >
+                <span className="flex gap-4">
+                  <span
+                    className="rounded-sm block w-6 h-6"
+                    style={{backgroundColor: item.color}}
+                  ></span>
+                  <p>{item.name}</p>
                 </span>
-
-                {/* <span className={`bg-[#00AD84] w-[24px] h-[24px] block rounded-[2px] `}></span> */}
-
-                <p>{transaction.name}</p>
-              </td>
-              <td>{transaction.total}</td>
-            </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td>Expenses:</td>
-            <td>{-totalExpense}</td>
-          </tr>
-          <tr>
-            <td>Income:</td>
-            <td>{totalIncome}</td>
-          </tr>
-        </tfoot>
-      </table>
+                <p>{item.type === "INCOME" ? item.total : -item.total}</p>
+              </li>
+            ))}
+          </ul>
+          <ul className="px-4 text-[0.875rem] font-semibold flex flex-col gap-[19px]">
+            <li className="flex justify-between">
+              <p>Expenses:</p>
+              <p style={{color: "var(--dashboard-text-color)"}}>{expenseSum}</p>
+            </li>
+            <li className="flex justify-between">
+              <p>Income:</p>
+              <p style={{color: "var(--yellow-color)"}}>{incomeSum}</p>
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
