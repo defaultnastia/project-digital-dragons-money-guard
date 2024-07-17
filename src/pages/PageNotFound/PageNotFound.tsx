46fa8448cfae346a72429c75215d3318477d1881
defaultnastia <anastayaaa@gmail.com>
2024-07-17 21:05:31 +0200

    added tailwind styles

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
-import css from "./PageNotFound.module.css";
 
 const PageNotFound = () => {
   return (
-    <div className={css.pageNotFound}>
-      <div className="flex flex-col gap-2 justify-center items-center">
-        <p className="bg-notFound bg-[size:100%] bg-clip-text bg-center text-[180px] font-black max-w-[700px]">
+    <div className="w-screen h-screen bg-[var(--white-color)] text-center text-[#180243] flex justify-center">
+      <div className="flex flex-col gap-2 justify-center items-center max-w-[700px]">
+        <p className="bg-notFound bg-[size:100%] bg-clip-text text-transparent bg-center text-[160px] font-black">
           Oops!
         </p>
-        <h2>404 - Page not found</h2>
-        <p className={css.helpText}>
+        <h2 className="uppercase font-semibold">404 - Page not found</h2>
+        <p>
           The page you are looking for might have been removed, or had its name
           changed, or is temporarily unavailable.
         </p>
