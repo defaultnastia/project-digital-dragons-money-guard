4542e39d4283d385fb2ae4cacba0bd5adca46a9e
LieraBerh <lierrastern@gmail.com>
2024-07-17 11:41:59 +0200

    add mock error and loading

--- src/components/CurrencyTab/CurrencyTab.tsx
+++ src/components/CurrencyTab/CurrencyTab.tsx
@@ -41,90 +41,97 @@ const CurrencyTab: React.FC = () => {
   };
 
   return (
-    <div className="flex-col w-[320px] md:w-[336px] xl:w-[480px] bg-[rgba(74,86,226,0.1)] rounded-br-[8px] rounded-bl-[8px] xl:rounded-none overflow-hidden">
+    <div>
       {loading ? (
-        <p className="flex items-center justify-center">Loading...</p>
+        <p className="flex items-center justify-center h-[214px] xl:h-[380px] w-[320px] md:w-[336px] xl:w-[480px]">
+          Loading...
+        </p>
       ) : error ? (
-        <p className="flex items-center justify-center">
+        <p className="flex items-center justify-center h-[214px] xl:h-[380px] w-[320px] md:w-[336px] xl:w-[480px]">
           Error fetching currency rates
         </p>
       ) : (
-        <table className="text-[16px] w-full table-fixed xl:mb-[36px]">
-          <thead className="text-[16px] bg-[rgba(255,255,255,0.20)] w-full">
-            <tr>
-              <th
-                className="pt-[12px] md:pt-[13px] xl:pt-[16px] pb-[12px] md:pb-[13px] xl:pb-[16px] pl-[20px] xl:pl-[96px]"
-                align="left"
-              >
-                Currency
-              </th>
-              <th
-                className="pt-[12px] md:pt-[13px] xl:pt-[16px] pb-[12px] md:pb-[13px] xl:pb-[16px]"
-                align="center"
-              >
-                Purchase
-              </th>
-              <th
-                className="pt-[12px] md:pt-[13px] xl:pt-[16px] pb-[12px] md:pb-[13px] xl:pb-[16px] pr-[20px] xl:pr-[96px]"
-                align="right"
-              >
-                Sale
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="h-[8px] xl:h-[24px] bg-transparent"></tr>
-            {currencyRates?.map((rate, index) => (
-              <tr key={index} className={index === 1 ? s.noPaddingBottom : ""}>
-                <td
-                  className={clsx(
-                    "pb-[12px] xl:pb-[24px] pl-[20px] xl:pl-[96px]",
-                    {
-                      [s.noBottomPadding]: index === 1,
-                    }
-                  )}
+        <div className="flex-col w-[320px] md:w-[336px] xl:w-[480px] bg-[rgba(74,86,226,0.1)] rounded-br-[8px] rounded-bl-[8px] xl:rounded-none overflow-hidden">
+          <table className="text-[16px] w-full table-fixed xl:mb-[36px]">
+            <thead className="text-[16px] bg-[rgba(255,255,255,0.20)] w-full">
+              <tr>
+                <th
+                  className="pt-[12px] md:pt-[13px] xl:pt-[16px] pb-[12px] md:pb-[13px] xl:pb-[16px] pl-[20px] xl:pl-[96px]"
                   align="left"
                 >
-                  {getCurrencyName(rate.currencyCodeA)}
-                </td>
-                <td
-                  className={clsx("pb-[12px] xl:pb-[24px]", {
-                    [s.noBottomPadding]: index === 1,
-                  })}
+                  Currency
+                </th>
+                <th
+                  className="pt-[12px] md:pt-[13px] xl:pt-[16px] pb-[12px] md:pb-[13px] xl:pb-[16px]"
                   align="center"
                 >
-                  {rate.rateBuy.toFixed(2)}
-                </td>
-                <td
-                  className={clsx(
-                    "pb-[12px] xl:pb-[24px] pr-[20px] xl:pr-[96px]",
-                    {
-                      [s.noBottomPadding]: index === 1,
-                    }
-                  )}
+                  Purchase
+                </th>
+                <th
+                  className="pt-[12px] md:pt-[13px] xl:pt-[16px] pb-[12px] md:pb-[13px] xl:pb-[16px] pr-[20px] xl:pr-[96px]"
                   align="right"
                 >
-                  {rate.rateSell.toFixed(2)}
-                </td>
+                  Sale
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-      <div className="h-[97px] xl:h-[192px]">
-        <div className={clsx("relative", s.chart)}>
-          {currencyRates && currencyRates[0] && (
-            <p className="hidden xl:block text-[#FF868D] text-[12px] absolute left-[47px] top-[12px]">
-              {currencyRates[0].rateBuy.toFixed(2)}
-            </p>
-          )}
-          {currencyRates && currencyRates[1] && (
-            <p className="hidden xl:block text-[#FF868D] text-[12px] absolute right-[96px] top-[-22px]">
-              {currencyRates[1].rateBuy.toFixed(2)}
-            </p>
-          )}
+            </thead>
+            <tbody>
+              <tr className="h-[8px] xl:h-[24px] bg-transparent"></tr>
+              {currencyRates?.map((rate, index) => (
+                <tr
+                  key={index}
+                  className={index === 1 ? s.noPaddingBottom : ""}
+                >
+                  <td
+                    className={clsx(
+                      "pb-[12px] xl:pb-[24px] pl-[20px] xl:pl-[96px]",
+                      {
+                        [s.noBottomPadding]: index === 1,
+                      }
+                    )}
+                    align="left"
+                  >
+                    {getCurrencyName(rate.currencyCodeA)}
+                  </td>
+                  <td
+                    className={clsx("pb-[12px] xl:pb-[24px]", {
+                      [s.noBottomPadding]: index === 1,
+                    })}
+                    align="center"
+                  >
+                    {rate.rateBuy.toFixed(2)}
+                  </td>
+                  <td
+                    className={clsx(
+                      "pb-[12px] xl:pb-[24px] pr-[20px] xl:pr-[96px]",
+                      {
+                        [s.noBottomPadding]: index === 1,
+                      }
+                    )}
+                    align="right"
+                  >
+                    {rate.rateSell.toFixed(2)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="h-[97px] xl:h-[192px]">
+            <div className={clsx("relative", s.chart)}>
+              {currencyRates && currencyRates[0] && (
+                <p className="hidden xl:block text-[#FF868D] text-[12px] absolute left-[47px] top-[12px]">
+                  {currencyRates[0].rateBuy.toFixed(2)}
+                </p>
+              )}
+              {currencyRates && currencyRates[1] && (
+                <p className="hidden xl:block text-[#FF868D] text-[12px] absolute right-[96px] top-[-22px]">
+                  {currencyRates[1].rateBuy.toFixed(2)}
+                </p>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
